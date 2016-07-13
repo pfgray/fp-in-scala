fd569e40a291b74af47af0f7804d95d7e08c9128
Paul Gray <pfbgray@gmail.com>
2016-07-12 23:08:02 -0400

    Adding test for solution #3.25

---
+++ src/test/scala/chapter3/TreeTest.scala
@@ -0,0 +1,31 @@
+package chapter3
+
+import org.scalatest.{Matchers, FlatSpec}
+
+/**
+ * Created by nicole on 7/12/16.
+ */
+class TreeTest extends FlatSpec with Matchers {
+
+  val testTree: Tree[Int] =
+    Branch(
+      Branch(
+        Branch(
+          Leaf(5),
+          Leaf(6)
+        ),
+        Leaf(8)
+      ),
+      Branch(
+        Leaf(9),
+        Leaf(10)
+      )
+    )
+
+  "Tree" should "compute the size of a tree correctly" in {
+    Tree.size(testTree) should equal(9)
+
+    Tree.size(Leaf("test")) should equal(1)
+  }
+
+}
