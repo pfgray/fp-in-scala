dd9ba4c76c7e605178e23eec690cfe212a79fb2a
Paul Gray <pfbgray@gmail.com>
2016-07-15 18:19:44 -0400

    Adding solution for #5.4

--- src/main/scala/chapter5/Stream.scala
+++ src/main/scala/chapter5/Stream.scala
@@ -29,6 +29,26 @@ sealed trait Stream[+A] {
       case (n, Cons(h, t)) => t().drop(n - 1)
       case _ => Empty
     }
+
+  def exists(p: A => Boolean): Boolean =
+    this match {
+      case Cons(h, t) => p(h()) || t().exists(p)
+      case _ => false
+    }
+
+  def foldRight[B](z: => B)(f: (A, => B) => B): B =
+    this match {
+      case Cons(h, t) => f(h(), t().foldRight(z)(f))
+      case _ => z
+    }
+
+  def exists2(p: A => Boolean): Boolean =
+    foldRight(false)((a, b) => p(a) || b)
+
+  // 5.4
+  def forAll(p: A => Boolean): Boolean =
+    foldRight(true)((a, b) => p(a) && b)
+
 }
 
 case object Empty extends Stream[Nothing]
@@ -48,5 +68,3 @@ object Stream {
     if(as.isEmpty) empty else cons(as.head, apply(as.tail: _*))
 
 }
-
-
--- src/test/scala/chapter5/StreamTest.scala
+++ src/test/scala/chapter5/StreamTest.scala
@@ -13,18 +13,25 @@ class StreamTest extends FlatSpec with Matchers {
     intStream.toList should equal(List(1, 2, 3, 4, 5))
   }
 
-  "Stream" should "take correctly" in {
+  it should "take correctly" in {
     intStream.take(2).toList should equal(List(1, 2))
 
     Stream("one", "two", "three", "four", () => throw new Exception("shouldn't fail"))
       .take(4).toList should equal(List("one", "two", "three", "four"))
   }
 
-  "Stream" should "drop correctly" in {
+  it should "drop correctly" in {
     intStream.drop(3).toList should equal(List(4, 5))
 
     Stream("one", "two", () => throw new Exception("shouldn't fail"), "four", "five", "six")
       .drop(3).toList should equal(List("four", "five", "six"))
+  }
+
+  it should "forAll correctly" in {
+    intStream.forAll(_ < 10) should equal(true)
+
+    Stream(1, 2, () => throw new Exception("shouldn't fail"))
+      .forAll(_.toString == "1") should equal(false)
   }
 
 }
