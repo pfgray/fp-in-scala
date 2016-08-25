84b39d7433a50d49b9606ecd82015c2533cd6de0
Paul Gray <pfbgray@gmail.com>
2016-08-25 00:46:28 -0400

    merging

--- src/main/scala/chapter7/Par.scala
+++ src/main/scala/chapter7/Par.scala
@@ -25,12 +25,13 @@ object Par {
     }
 
   def fork[A](a: => Par[A]): Par[A] =
-    es => es.submit(new Callable[A] {
-      override def call(): A = a(es).get
-    })
+    es =>
+      es.submit(new Callable[A] {
+        override def call(): A = a(es).get
+      })
 
   // 7.4
-  def asyncF[A,B](f: A => B): A => Par[B] = a => fork(unit(f(a)))
+  def asyncF[A, B](f: A => B): A => Par[B] = a => fork(unit(f(a)))
 
   // 7.5
   def sequence[A](ps: List[Par[A]]): Par[List[A]] =
@@ -40,16 +41,39 @@ object Par {
 
   // 7.6
   def parFilter[A](as: List[A])(f: A => Boolean): Par[List[A]] = {
-    as.foldRight(unit(Nil: List[A])) { (a, pList) =>
-      map2(pList, unit(a)) { (ls, a) =>
-        if(f(a)) a :: ls else ls
-      }
-    }
+//    as.foldRight(unit(Nil: List[A])) { (a, pList) =>
+//      map2(pList, unit(a)) { (ls, a) =>
+//        if(f(a)) a :: ls else ls
+//      }
+//    }
+    val pars = as.map(asyncF(a => if (f(a)) List(a) else List()))
+    sequence(pars) map (_.flatten)
   }
 
   implicit class ParExtensions[A](a: Par[A]) {
     def map[B](f: A => B): Par[B] =
       map2(a, unit())((a, _) => f(a))
+  }
+
+  implicit class ParIndexedSeqExtensions[A](as: IndexedSeq[A]) {
+    // 7.8
+    def parFold(z: A)(f: (A, A) => A): Par[A] =
+      if (as.length <= 1) {
+        Par.unit[A](as.headOption.getOrElse(z))
+      } else {
+        val (l, r) = as.splitAt(as.length / 2)
+        Par.map2(Par.fork(l.parFold(z)(f)), Par.fork(r.parFold(z)(f)))(f)
+      }
+  }
+
+  val maxer = (a: Int, b: Int) => {
+    println(s"comparing: $a, & $b")
+    a max b
+  }
+
+  implicit class ParIndexedSeqIntExtensions(as: IndexedSeq[Int]) {
+    def parMax: Par[Int] =
+      as.parFold(0)(maxer) // (_ max _)
   }
 
 }
--- src/test/scala/chapter7/ParTest.scala
+++ src/test/scala/chapter7/ParTest.scala
@@ -3,7 +3,7 @@ package chapter7
 import java.util.concurrent.{Executors, ForkJoinPool, ExecutorService}
 
 import org.scalatest.{Matchers, FlatSpec}
-import chapter7.Par.ParExtensions
+import chapter7.Par.{ParExtensions, ParIndexedSeqIntExtensions}
 
 /**
  * Created by nicole on 8/4/16.
@@ -30,6 +30,22 @@ class ParTest extends FlatSpec with Matchers {
     val res = filterPar(Executors.newFixedThreadPool(4))
     val filtered = res.get()
     filtered should equal(List(8, 9, 10))
+  }
+
+  it should "dead-lock with certain fixed sized threadpools" in {
+    val things = List(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+
+    val filterPar = Par.parFilter(things)(_ > 7)
+
+    val res = filterPar(Executors.newFixedThreadPool(4))
+    val filtered = res.get()
+    filtered should equal(List(8, 9, 10))
+  }
+
+  it should "do other stuff" in {
+    val thing = IndexedSeq(4, 5, 3, 6, 8, 9, 12, 4, 5, 2, 1)
+
+    thing.parMax(new ForkJoinPool(10))
   }
 
 }
